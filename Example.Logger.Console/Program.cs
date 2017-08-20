5b3d1d694ba4596de5dc1bf8b6f72477380e193c
Alger <AlgerDu@outlook.com>
2017-08-20 23:12:16 +0800

    example 编写，有错误

--- Example.Logger.Console/Program.cs
+++ Example.Logger.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Autofac;
+using D.Util.Interface;
+using D.Util.Logger;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,25 @@ namespace Example.Logger.Console
     {
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<ConsoleLogWriter>()
+                .As<ILogWriter>();
+
+            builder.RegisterType<LoggerFactory>()
+                .As<ILoggerFactory>();
+
+            var container = builder.Build();
+
+            var loggerFactory = container.Resolve<ILoggerFactory>();
+
+            var logger = loggerFactory.CreateLogger<Program>();
+
+            var name = "日志";
+
+            logger.LogDebug($"helle debug {name}");
+
+            System.Console.ReadKey();
         }
     }
 }
