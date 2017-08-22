8f0abd541f36e2569921a2c73125eb403f09958b
Alger <AlgerDu@outlook.com>
2017-08-22 13:46:24 +0800

    接口定义和部分接口实现

---
+++ D.Util/Config/NullConfigProvider.cs
@@ -0,0 +1,21 @@
+﻿using D.Util.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Util.Config
+{
+    /// <summary>
+    /// 使用 NullObject 代替 null
+    /// </summary>
+    public class NullConfigProvider : IConfigProvider
+    {
+        T IConfigProvider.GetConfig<T>(string instanceName)
+        {
+            //永远都返回一个 null 值的配置对象
+            return null;
+        }
+    }
+}
--- D.Util/Interface/Config/IConfigCollector.cs
+++ D.Util/Interface/Config/IConfigCollector.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace D.Util.Interface
 {
     /// <summary>
-    /// 配置收集器
+    /// 配置收集器（设计思路学习 autofac）
     /// </summary>
     public interface IConfigCollector
     {
@@ -16,5 +16,18 @@ namespace D.Util.Interface
         /// </summary>
         /// <returns></returns>
         IConfigProvider CreateProvider();
+
+        /// <summary>
+        /// 像 collector 中手动添加一个配置对象
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="instanceName"></param>
+        void CollectConfig(IConfig config, string instanceName = null);
+
+        /// <summary>
+        /// 像 collector 中添加一个 loader，用于获取配置
+        /// </summary>
+        /// <param name="loader"></param>
+        void AddLoader(IConfigLoader loader);
     }
 }
