77de3ec13a4a8bec833f27d91edf0ac0ed5f6502
Alger <AlgerDu@outlook.com>
2017-09-03 23:06:12 +0800

    注释修改

--- D.Util/Interface/Config/IConfigProvider.cs
+++ D.Util/Interface/Config/IConfigProvider.cs
@@ -12,11 +12,11 @@ namespace D.Util.Interface
     public interface IConfigProvider
     {
         /// <summary>
-        /// 获取配置
+        /// 获取配置，当没有从配置中找到对应的项的时候，返回 null
         /// </summary>
         /// <typeparam name="T">具体的配置类</typeparam>
         /// <param name="instanceName">实例名</param>
         /// <returns></returns>
-        T GetConfig<T>(string instanceName = null) where T : class, IConfig, new;
+        T GetConfig<T>(string instanceName = null) where T : class, IConfig, new();
     }
 }
