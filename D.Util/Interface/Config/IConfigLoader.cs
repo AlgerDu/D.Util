40f38b292a51bf770677bc286c724960b2907e62
Alger <AlgerDu@outlook.com>
2017-09-09 18:33:35 +0800

    loader 接口修改

--- D.Util/Interface/Config/IConfigLoader.cs
+++ D.Util/Interface/Config/IConfigLoader.cs
@@ -14,10 +14,9 @@ namespace D.Util.Interface
         /// <summary>
         /// 从加载器中获取配置
         /// </summary>
-        /// <param name="path">类似与 XML 中的 path</param>
         /// <param name="instanceName">实例名（为多实例做准备）</param>
         /// <returns></returns>
-        T Load<T>(string path, string instanceName = null) where T : class, IConfig, new();
+        T Load<T>(string instanceName = null) where T : class, IConfig, new();
 
         /// <summary>
         /// 保存所有从此 loader 加载的配置项，思考半天，觉得还是需要个保存的接口
