429e8d4be62f13ab6915626d419865fafb3cdfdd
Alger <AlgerDu@outlook.com>
2018-04-07 14:18:39 +0800

    添加文件，实现一个 provider 基类

---
+++ D.Utils.Extensions.Logging.RollingFile/DRollingFileOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Utils.Extensions.Logging.RollingFile
+{
+    public class DBaseProviderOptions
+    {
+
+    }
+
+    class DRollingFileOptions
+    {
+    }
+}
---
+++ D.Utils.Extensions.Logging.RollingFile/LogContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Utils.Extensions.Logging.RollingFile
+{
+    /// <summary>
+    /// 日志内容
+    /// </summary>
+    public class LogContent
+    {
+    }
+}
