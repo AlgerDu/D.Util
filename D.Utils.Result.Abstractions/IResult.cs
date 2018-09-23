a4b8d9df8da4f3d144f879e6528bbc1e8d14dbc4
Alger <AlgerDu@outlook.com>
2018-09-23 15:47:35 +0800

    定义新的接口

---
+++ D.Utils.Result.Abstractions/IResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Utils
+{
+    /// <summary>
+    /// 通用结果
+    /// </summary>
+    public interface IResult
+    {
+        /// <summary>
+        /// 结果标识
+        /// </summary>
+        int Code { get; }
+
+        /// <summary>
+        /// 返回信息，成功与否都可能有消息返回
+        /// </summary>
+        string Msg { get; }
+
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        /// <returns></returns>
+        bool IsSuccess();
+    }
+
+    /// <summary>
+    /// 通用结果
+    /// </summary>
+    public interface IResult<T>
+        : IResult
+    {
+        /// <summary>
+        /// 泛型数据，默认为 defaul(T)
+        /// </summary>
+        T Data { get; }
+    }
+}
---
+++ D.Utils.Result.Abstractions/ISearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Utils
+{
+    /// <summary>
+    /// 分页查询结果
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface ISearchResult<T> : IResult
+    {
+        /// <summary>
+        /// 每页数量，为 -1 时代表不分页
+        /// </summary>
+        /// <returns></returns>
+        long PageSize { get; }
+
+        /// <summary>
+        /// 页码，不分页时返回 1
+        /// </summary>
+        /// <returns></returns>
+        long PageIndex { get; }
+
+        /// <summary>
+        /// 总记录数
+        /// </summary>
+        /// <returns></returns>
+        long RecodCount { get; }
+
+        /// <summary>
+        /// 列表数据
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> Datas { get; }
+    }
+}
