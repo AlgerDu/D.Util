84cc2af34ea4ecc5fc5e8e739bc66ae546b5394a
Alger <AlgerDu@outlook.com>
2018-10-09 23:26:42 +0800

    添加接口和包文件

---
+++ D.Utils.Application.Abstractions/IApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Utils
+{
+    /// <summary>
+    /// App
+    /// </summary>
+    public interface IApplication
+    {
+        /// <summary>
+        /// 启动
+        /// </summary>
+        /// <returns></returns>
+        IApplication Run();
+
+        /// <summary>
+        /// 停止
+        /// </summary>
+        /// <returns></returns>
+        IApplication Stop();
+    }
+}
---
+++ D.Utils.Application.Abstractions/IApplicationBuilder.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Utils
+{
+    /// <summary>
+    /// app 构建器
+    /// </summary>
+    public interface IApplicationBuilder
+    {
+        /// <summary>
+        /// 使用 .net core 自带的容器
+        /// </summary>
+        IServiceCollection Services { get; }
+
+        /// <summary>
+        /// 使用 Startup
+        /// </summary>
+        /// <typeparam name="Startup"></typeparam>
+        /// <returns></returns>
+        IApplicationBuilder Use<Startup>() where Startup : IStartup;
+
+        /// <summary>
+        /// 构建 app
+        /// </summary>
+        /// <typeparam name="App"></typeparam>
+        /// <returns></returns>
+        App Builde<App>() where App : IApplication;
+    }
+}
