00a5f5f2ad8b8189511209a5ba48f01ba4e2020e
Alger <AlgerDu@outlook.com>
2019-06-08 18:06:22 +0800

    添加一个模板项目，也做测试用

---
+++ Examples/D.Examples.App.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D.Examples.App.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
