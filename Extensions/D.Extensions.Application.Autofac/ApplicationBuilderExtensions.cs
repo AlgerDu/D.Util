b268bf009483debc003f6f304f87fe2e74e70f6a
Alger <AlgerDu@outlook.com>
2019-07-18 09:21:29 +0800

    修改对 startup 的使用方式

---
+++ Extensions/D.Extensions.Application.Autofac/ApplicationBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using D.Infrastructures.Application;
+
+namespace D.Infrastructures
+{
+    /// <summary>
+    /// IApplicationBuilder 的一些扩展方法
+    /// </summary>
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// 使用 Startup 类，可以配合使用 autofac
+        /// </summary>
+        /// <param name="applicationBuilder"></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseStartupWithAutofac<T>(
+            this IApplicationBuilder applicationBuilder
+            )
+            where T : class
+        {
+            applicationBuilder.ConfigureServices(collection =>
+            {
+                collection.AddTransient<T>();
+
+                var provider = collection.BuildServiceProvider();
+
+                var startupInstance = provider.GetService<T>();
+
+
+
+                typeof(T).InvokeMember(
+                    "ConfigServices"
+                    , System.Reflection.BindingFlags.InvokeMethod
+                    , null
+                    , startupInstance
+                    , new object[] { collection }
+                    );
+            });
+
+            return applicationBuilder;
+        }
+    }
+}
